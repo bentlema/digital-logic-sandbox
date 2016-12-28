e99205f4baec1248431ce8c1e469691470841052
Mark Bentley <mark.bentley8@t-mobile.com>
2016-12-28 12:52:47 -0800

    add orGate() class

--- gate.py
+++ gate.py
@@ -61,3 +61,31 @@ class andGate(simpleGate):
         print("{!r:<5} AND {!r:<5} = {!r:<5}".format(self.inputConnection['IN_0'].state,self.inputConnection['IN_1'].state,self.outputConnection['OUT_0'].state))
         print()
 
+
+class orGate(simpleGate):
+
+    def __init__(self):
+
+        # Dictionaries to keep track of inputs and outputs
+        self.inputConnection = {}
+        self.outputConnection = {}
+
+        # An OR gate has two input connections and one output connection
+        self.inputConnection['IN_0'] = inputConnection('Input 0')
+        self.inputConnection['IN_1'] = inputConnection('Input 1')
+        self.outputConnection['OUT_0'] = outputConnection('Output')
+
+    def updateState(self):
+        self.outputConnection['OUT_0'].state = self.inputConnection['IN_0'].state or self.inputConnection['IN_1'].state
+
+    #
+    # Need to re-write this in a general/generic way, without any knowledge of the input/output names
+    # so that we can "promote" this method up to to the simpleGate class
+    #
+    def printState(self):
+        print()
+        print("IN_0      IN_1    OUT_0")
+        print("-----     -----   -----")
+        print("{!r:<5} OR {!r:<5} = {!r:<5}".format(self.inputConnection['IN_0'].state,self.inputConnection['IN_1'].state,self.outputConnection['OUT_0'].state))
+        print()
+
