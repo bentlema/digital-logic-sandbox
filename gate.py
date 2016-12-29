862e2dca81b7e7281bf1a13c0cf23daef5ef228d
Mark Bentley <mark.bentley8@t-mobile.com>
2016-12-29 15:52:35 -0800

    started to implement truth table

--- gate.py
+++ gate.py
@@ -4,6 +4,8 @@ Created on Dec 26, 2016
 @author: bentlema
 '''
 
+import math
+
 class inputConnection():
     def __init__(self,label):
         self.state = bool(False)
@@ -18,11 +20,36 @@ class outputConnection():
 
 class logicGate():
 
-    def printState(self):
+    def printTruthTable(self):
+        numberOfInputs = len(self.inputConnection)
+        # Think of number of inputs as a binary string of bits
+        # 1 input requires 1 bit
+        # 2 inputs requires 2 bits
+        # 3 inputs requires 3 bits, etc.
+        inputPermutations = int(math.pow(2, numberOfInputs))  # all possible input combinations
+        formatString = "{:0" + str(numberOfInputs) + "b}"     # binary number stringified
+        for i in range(inputPermutations):
+            myBits = formatString.format(i)
+            '''
+                take a look at http://stackoverflow.com/questions/1679384/converting-python-dictionary-to-list
+                we have the input values being generated now, but still need to set those input values and
+                calculate the output value, and print.  Since we refer the inputConnections by name, we need
+                to create a mapping of connection names to connection number or something...either that, or
+                we need to reconsider storing the connection names as a dict, and maybe a list/array would
+                be more convenient...not sure yet. Maybe there's a way to store it both ways?
+            '''
+            if (numberOfInputs == 1):
+                print(myBits[0], outputValue)
+            elif (numberOfInputs == 2):
+                print(myBits[0], myBits[1], outputValue)
+            elif (numberOfInputs == 3):
+                print(myBits[0], myBits[1], myBits[2], outputValue)
+            print()
 
+
+    def printState(self):
         for i in self.inputConnection.keys():
             print("{} = {}".format(self.inputConnection[i].label,self.inputConnection[i].state))
-
         for o in self.outputConnection.keys():
             print("{} = {}".format(self.outputConnection[o].label,self.outputConnection[o].state))
 
