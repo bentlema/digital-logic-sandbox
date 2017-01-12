5d3b3483d31dc22301b2e92407d40cab6b9bf295
Mark Bentley <mark.bentley8@t-mobile.com>
2017-01-11 19:38:55 -0800

    test every input combination for the full adder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 from gate import logicGate, andGate, orGate, xorGate, notGate, bufferGate, wire
 
 #
@@ -51,12 +52,12 @@ switchA = bufferGate()
 switchB = bufferGate()
 #switchC = circuitInput('IN_C', 'toggle') # input Carry
 switchC = bufferGate()
-
 #lampS = circuitOutput('OUT_S') # shown in GUI as a lamp/LED
 lampS = bufferGate()
 #lampC = circuitOutput('OUT_C') # shown in GUI as a lamp/LED
 lampC = bufferGate()
 
+# connect all the wires to the gates to make a full adder with carry
 wire1.connect(switchA.outputConnection['OUT_0'],  xor1.inputConnection['IN_0'])
 wire2.connect(switchB.outputConnection['OUT_0'],  xor1.inputConnection['IN_1'])
 wire3.connect(xor1.outputConnection['OUT_0'],     xor2.inputConnection['IN_0'])
@@ -71,71 +72,42 @@ wire11.connect(and2.outputConnection['OUT_0'],    or1.inputConnection['IN_0'])
 wire12.connect(or1.outputConnection['OUT_0'],     lampC.inputConnection['IN_0'])
 
 # Try toggling these 3 inputs, and you'll see that this full adder works
-switchA.toggleInputStateByName('IN_0')
-switchB.toggleInputStateByName('IN_0')
-switchC.toggleInputStateByName('IN_0')
+#switchA.toggleInputStateByName('IN_0')
+#switchB.toggleInputStateByName('IN_0')
+#switchC.toggleInputStateByName('IN_0')
 
-print()
-print("switchA")
-switchA.printState()
+switchA.setInputStateByName('IN_0', bool(0))
+switchB.setInputStateByName('IN_0', bool(0))
+switchC.setInputStateByName('IN_0', bool(0))
 
-print()
-print("switchB")
-switchB.printState()
+numberOfInputs = 3                                    # The full adder with carry has 3 inputs
+inputPermutations = int(math.pow(2, numberOfInputs))  # all possible input combinations
+formatString = "{:0" + str(numberOfInputs) + "b}"     # binary number stringified
+for i in range(inputPermutations):
+    myBits = formatString.format(i)
+    a = bool(int(myBits[0])) # take single character out of string (which will be a 0 or 1
+    b = bool(int(myBits[1])) # and convert to an int, and then convert to a boolean
+    c = bool(int(myBits[2]))
+    print("SwitchA {},  SwitchB {},  SwitchC {}".format(a, b, c))
+    switchA.setInputStateByName('IN_0', a)
+    switchB.setInputStateByName('IN_0', b)
+    switchC.setInputStateByName('IN_0', c)
 
-print()
-print("switchC")
-switchC.printState()
+    # we call updateState() enough times to propagate the signal through the entire circuit
+    counter = 100
+    while counter > 0:
+        for obj in circuit1:
+            obj.updateState()
+        counter -= 1
+    
+    print()
+    print("lampS (Sum)")
+    lampS.printState()
 
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-for obj in (and1, and2, xor1, xor2, or1, wire1, wire2, wire3, wire4, wire5, wire6, wire7, wire8, wire9, wire10, wire11, wire12):
-    obj.updateState()
-    
-print()
-print("lampS")
-lampS.printState()
-
-print()
-print("lampC")
-lampC.printState()
+    print()
+    print("lampC (Carry)")
+    lampC.printState()
+    print()
 
 
 
